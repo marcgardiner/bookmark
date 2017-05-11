7611aa2d464277313ab5f4264b4cc583a861060a
service <servicemanager3386@yandex.com>
2017-05-11 12:27:42 -0400

    # created new  - user profile

--- account/urls.py
+++ account/urls.py
@@ -5,10 +5,14 @@ from . import views
 urlpatterns = [
     # post views
     # url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', views.dashboard, name='dashboard'),
+
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
-    url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^edit/$', views.eidt, name='edit'),
+
     url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
     url(r'^password-reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
@@ -17,5 +21,4 @@ urlpatterns = [
         'django.contrib.auth.views.password_reset_confirm',
         name='password_reset_confirm'),
     url(r'^password-reset/complete/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
-    url(r'^register/$', views.register, name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -29,7 +30,24 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile,
+                                       data=request.POST,
+                                       files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
 
+    return render(request,
+                  'account/edit.html',
+                  {'user_form': user_form,
+                   'profile_form': profile_form})
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
@@ -37,6 +55,7 @@ def register(request):
             new_user = user_form.save(commit=False)
             new_user.set_password(user_form.cleaned_data['password'])
             new_user.save()
+            profile = Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
