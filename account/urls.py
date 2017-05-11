984b275e8c6df457192d2b7ee615096f9ce411ac
service <servicemanager3386@yandex.com>
2017-05-11 13:06:38 -0400

    # modified  - user profile and message routine

--- account/urls.py
+++ account/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^edit/$', views.eidt, name='edit'),
+    url(r'^edit/$', views.edit, name='edit'),
 
     url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
     url(r'^password-change/done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done'),
