d9c94fa54e7c354f4063dea9d1db7519217ded38
service <servicemanager3386@yandex.com>
2017-05-11 11:33:26 -0400

    # issue  - registration issue is fixed.

--- account/forms.py
+++ account/forms.py
@@ -17,4 +17,4 @@ class UserRegistrationForm(forms.ModelForm):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
-        return cd[password2]
+        return cd['password2']
