6b4944553891b8377f2a72c9df9b499d41687788
service <servicemanager3386@yandex.com>
2017-05-13 11:07:51 -0400

    # created  - bookmark image detail

--- images/views.py
+++ images/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+
 from .forms import ImageCreateForm
+from .models import Image
 
 @login_required
 def image_create(request):
@@ -13,10 +15,17 @@ def image_create(request):
             new_item.user = request.user
             new_item.save()
             messages.success(request, 'Image added successfully')
-            return redirect(new_item.get_obsolute_url())
+            return redirect(new_item.get_absolute_url())
     else:
         form = ImageCreateForm(data=request.GET)
     return render(request,
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
